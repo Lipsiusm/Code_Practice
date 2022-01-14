4044789afe2c23d234c1d726b6c08d35c0e272d4
Lipsiusm <lipsius.m@gmail.com>
2022-01-13 22:54:49 -0700

    Python practice, reverse words >= 5 chars

---
+++ spin_words.py
@@ -0,0 +1,29 @@
+#Write a function that takes in a string of one or more words, and returns the same string, 
+#but with all five or more letter words reversed (Just like the name of this Kata). 
+#Strings passed in will consist of only letters and spaces. Spaces will be included
+#only when more than one word is present.
+
+#Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+#spinWords( "This is a test") => returns "This is a test" 
+#spinWords( "This is another test" )=> returns "This is rehtona test"
+
+#I have included the question above the code, where I have only been provided with the method header.
+
+def spin_words(sentence):
+    
+    #splitting the string into a list
+    sentence = list(sentence.split())
+    flipped = ''
+    
+    #checking every list item to flip it based on length
+    #and concatenate the new string
+    for i in sentence:
+        if len(i) >= 5:
+            flipped = flipped + i [::-1] + ' '
+        else:
+            flipped = flipped + i + ' '
+            
+    #trimming the excess space off the string's last character
+    flipped = flipped[:-1]
+
+    return flipped
