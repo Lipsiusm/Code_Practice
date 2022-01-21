f4d2c877d0ef217201eab7cf9d352c2180e7c5a4
Matt <lipsius.m@gmail.com>
2022-01-20 20:32:35 -0700

    updated questions for LongestConsecutiveRep.java

--- LongestConsecutiveRep.java
+++ LongestConsecutiveRep.java
@@ -1,3 +1,17 @@
+/*For a given string s find the character c (or C) 
+with longest consecutive repetition and return:
+
+Object[]{c, l};
+
+where l (or L) is the length of the repetition. 
+If there are two or more characters with the same l 
+return the first in order of appearance.
+
+For empty string return:
+
+Object[]{"", 0}*/
+
+
 import java.util.*;
 
 public class LongestConsecutiveRep{
